3b24f8b3cc60bb273b6ba30641703df0c75c1612
Paul McCranie <paulmccranie@hotmail.com>
2019-06-12 18:47:35 -0500

    BrightShadowsDesign.com - added env variables to config.js

--- src/config.js
+++ src/config.js
@@ -1,17 +1,42 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-uploads"
+    REGION: "us-east-2",
+    BUCKET: "bright-shadows-design-api-dev-artworkbucket-i15nf9yhdoy6"
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://5by75p4gn3.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: "us-east-2",
+    URL: "https://l702bxynci.execute-api.us-east-2.amazonaws.com/dev"
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_udmFFSb92",
-    APP_CLIENT_ID: "4hmari2sqvskrup67crkqa4rmo",
-    IDENTITY_POOL_ID: "us-east-1:ceef8ccc-0a19-4616-9067-854dc69c2d82"
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_S35hiiZPX",
+    APP_CLIENT_ID: "1b3921bi5mbo3cfhkj6fbtbk4v",
+    IDENTITY_POOL_ID: "us-east-2:f4662a4b-24da-4691-b03e-a5fe6caf52e4"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "bright-shadows-design-api-prod-artworkbucket-5f5ic9d1zs2v"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://ba9j7ui4c9.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_Dg7iQFYsa",
+    APP_CLIENT_ID: "1r4a4vpurjemne5foda6lfi3c3",
+    IDENTITY_POOL_ID: "us-east-2:53f095bb-3edf-4fce-ad8e-515689f26d02"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === "prod" ? prod : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
